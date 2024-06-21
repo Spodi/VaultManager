3ba7da0b11fd49e643e652ef3aa6532a8919c621
Spodi <spodi2290@aol.com>
2024-06-21 17:49:36 +0200

    Make RegEx toggle invisible for mode "All"

--- VaultAssets/VaultManager.cs
+++ VaultAssets/VaultManager.cs
@@ -1,10 +1,11 @@
 
 using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using System.Reflection;
 using System.Windows.Data;
 using System.Windows.Forms;
-
 namespace CyberOasis.VaultManager
 {
     public class SizeConverter : IValueConverter
@@ -19,6 +20,36 @@ namespace CyberOasis.VaultManager
         {
             return double.Parse(value.ToString()) + double.Parse(parameter.ToString());
         }
+    }
+    public class InverseBooleanConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            return !(bool)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+
+    }
+    public class ValueConverterGroup : List<IValueConverter>, IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return this.Aggregate(value, (current, converter) => converter.Convert(current, targetType, parameter, culture));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
     }
     /// The original namespace was ErikE.Shuriken . So thanks to ErikE on StackOverflow!
     /// <summary>
