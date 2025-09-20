4bfbe3e780ef3ff28ff12d79fc1745d1d57477fe
Spodi <spodi2290@aol.com>
2025-09-20 12:49:22 +0200

    Update SpodiUtils.psm1

--- VaultAssets/SpodiUtils.psm1
+++ VaultAssets/SpodiUtils.psm1
@@ -1,4 +1,5 @@
 using namespace System.Collections.Generic
+
 [Flags()] enum FSEntryType {
     None = 0
     File = 1
@@ -6,47 +7,73 @@ using namespace System.Collections.Generic
     All = 3
 }
 
-enum SearchOption {
-    TopDirectoryOnly = 0
-    AllDirectories = 1
-}
 class FileSystemEntries {
    
-    static [List[string]] Get([string]$Path) {
-        return [FileSystemEntries]::Get($Path, [FSEntryType]::All, [SearchOption]::TopDirectoryOnly)
+    static [IEnumerable[string]] Get([string]$Path) {
+        return [FileSystemEntries]::Get($Path, '*', [FSEntryType]::All, [System.IO.SearchOption]::TopDirectoryOnly)
     }
 
-    static [List[string]] Get([string]$Path, [FSEntryType]$FSEntryType) {
-        return [FileSystemEntries]::Get($Path, $FSEntryType, [SearchOption]::TopDirectoryOnly)
+    static [IEnumerable[string]] Get([string]$Path, [string]$SearchPattern) {
+        return [FileSystemEntries]::Get($Path, $SearchPattern, [FSEntryType]::All, [System.IO.SearchOption]::TopDirectoryOnly)
+    }
+    static [IEnumerable[string]] Get([string]$Path, [FSEntryType]$FSEntryType) {
+        return [FileSystemEntries]::Get($Path, '*', $FSEntryType, [System.IO.SearchOption]::TopDirectoryOnly)
+    }
+    static [IEnumerable[string]] Get([string]$Path, [System.IO.SearchOption]$SearchOption) {
+        return [FileSystemEntries]::Get($Path, '*', [FSEntryType]::All, $SearchOption)
+    }
+
+    static [IEnumerable[string]] Get([string]$Path, [string]$SearchPattern, [FSEntryType]$FSEntryType) {
+        return [FileSystemEntries]::Get($Path, $SearchPattern, $FSEntryType, [System.IO.SearchOption]::TopDirectoryOnly)
+    }
+    static [IEnumerable[string]] Get([string]$Path, [string]$SearchPattern, [System.IO.SearchOption]$SearchOption) {
+        return [FileSystemEntries]::Get($Path, $SearchPattern, [FSEntryType]::All, $SearchOption)
+    }
+
+    static [IEnumerable[string]] Get([string]$Path, [FSEntryType]$FSEntryType, [System.IO.SearchOption]$SearchOption) {
+        return [FileSystemEntries]::Get($Path, '*', $FSEntryType, $SearchOption)
     }
         
-    static [List[string]] Get([string]$Path, [FSEntryType]$FSEntryType, [SearchOption]$SearchOption) {
+    static [IEnumerable[string]] Get([string]$Path, [string]$SearchPattern, [FSEntryType]$FSEntryType, [System.IO.SearchOption]$SearchOption) {
         $prevDir = [System.IO.Directory]::GetCurrentDirectory()
         [System.IO.Directory]::SetCurrentDirectory((Get-Location))
+
+        if ($SearchOption -eq [System.IO.SearchOption]::TopDirectoryOnly) {
+            switch ($FSEntryType) {
+                ([FSEntryType]::Directory) {
+                    return [List[string]]([System.IO.Directory]::EnumerateDirectories($Path, $SearchPattern) | ForEach-Object { $_ + [System.IO.Path]::DirectorySeparatorChar })
+                }
+                ([FSEntryType]::File) {
+                    return [System.IO.Directory]::EnumerateFiles($Path, $SearchPattern)
+                }
+            }
+        }
+
         $Queue = [System.Collections.Queue]@()
         $Output = [List[string]]::new()
         $Queue.Enqueue($Path)
         while ($Queue.count -gt 0) {
             try {
                 $Current = $Queue.Dequeue()
-                [System.IO.Directory]::EnumerateDirectories($Current) | & { process {
+                [System.IO.Directory]::EnumerateDirectories($Current, $SearchPattern) | & { process {
                         if ($FSEntryType.HasFlag([FSEntryType]::Directory) ) { $Output.Add($_ + [System.IO.Path]::DirectorySeparatorChar) }
                         if ($SearchOption) { $Queue.Enqueue($_) }
                     } }
-                if ($FSEntryType.HasFlag([FSEntryType]::File)) { $Output.AddRange([System.IO.Directory]::EnumerateFiles($Current)) }
+                if ($FSEntryType.HasFlag([FSEntryType]::File)) { $Output.AddRange([System.IO.Directory]::EnumerateFiles($Current, $SearchPattern)) }
             }
-            catch [System.Management.Automation.RuntimeException] {
+            catch [System.Management.Automation.RuntimeException], [System.Management.Automation.MethodInvocationException] {
                 $catchedError = $_
                 switch ($catchedError.Exception.InnerException.GetType().FullName) {
                     'System.UnauthorizedAccessException' { Write-Warning $catchedError.Exception.InnerException.Message }
                     'System.Security.SecurityException' { Write-Warning $catchedError.Exception.InnerException.Message }
+                    'System.IO.DirectoryNotFoundException' { Write-Warning $catchedError.Exception.InnerException.Message }
                     default {
-                        throw $catchedError
+                        throw
                     }
-                }  
-            }
-        
+                }
+            }    
         }
+
         [System.IO.Directory]::SetCurrentDirectory($prevDir)
         if ($Output.Count -eq 0) {
             return $null
@@ -83,7 +110,8 @@ function Get-FileSystemEntries {
     }
     if ($Recurse) {
         $SearchOption = [SearchOption]::AllDirectories
-    } else {
+    }
+    else {
         $SearchOption = [SearchOption]::TopDirectoryOnly
     }
     [FileSystemEntries]::Get($Path, $EntryType, $SearchOption) 
@@ -98,8 +126,8 @@ function Get-FolderSubs {
         [Parameter(Mandatory, ValueFromPipeline)]  [string] $Path
     )
     process {
-        [FileSystemEntries]::Get($Path, 'Directory', 'TopDirectoryOnly') | & { process {
-                [FileSystemEntries]::Get($_, 'Directory', 'TopDirectoryOnly') | Write-Output
+        [FileSystemEntries]::Get($Path, [FSEntryType]::Directory, [SearchOption]::TopDirectoryOnly) | & { process {
+                [FileSystemEntries]::Get($_, [FSEntryType]::Directory, [SearchOption]::TopDirectoryOnly) | Write-Output
             } }
     }
 }
